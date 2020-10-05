f5a6538cd6308d9f948ebedc649b38733f280342
mtoce <mtoce@villanova.edu>
2020-10-05 10:40:38 -0400

    included covid router

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.api import predict, viz, housing, jobs, weather, card_viz
+from app.api import predict, viz, housing, jobs, weather, covid, card_viz
 
 app = FastAPI(
     title='DRIFTLY DS API',
@@ -20,6 +20,7 @@ app.include_router(viz.router)
 app.include_router(housing.router)
 app.include_router(jobs.router)
 app.include_router(weather.router)
+app.include_router(covid.router)
 app.include_router(card_viz.router)
 
 app.add_middleware(
